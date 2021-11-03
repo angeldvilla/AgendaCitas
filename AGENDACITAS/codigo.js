dc8d0adea80eb6a8aeff0d6f0bc1a9572a085fa8
Angel David Villa <89151224+angeldvilla@users.noreply.github.com>
2021-11-02 19:22:30 -0500

    Add files via upload

---
+++ AGENDACITAS/codigo.js
@@ -0,0 +1,27 @@
+console.log("Hola mundo");
+console.log("En este documento se practicará algo de JavaScript :)");
+
+/* var numero, coma = ",";
+for (numero = 1; numero <= 10; numero++) {
+    if (numero % 2 == 0) {
+        console.log(numero + " Es par");
+    } else {
+        console.log(numero + " Es impar");
+    }
+}
+console.log("Aquí encontraremos algunas razas de perros por medio de una función.");
+function razas() {
+    races = ["Pitbull", "Golden Retriever", "Criollo", "Sabueso", "Doberman"]
+    console.log(races[1]);
+    races[1] = "Roco";
+    console.log(races[1]);
+}
+function ciclo() {
+    var x, n;
+    for (x = 1; x <= n; x += 2) {
+        console.log("Numero " + x);
+    }
+}
+razas();
+ciclo();
+*/
